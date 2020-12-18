16efa329d91c78c509525f4bc5996db5b052f730
Eric Chen <ericchen@Erics-MacBook-Pro-16.local>
2020-12-18 14:41:40 -0500

    bug fixes

--- Inc.Cure/GUI.java
+++ Inc.Cure/GUI.java
@@ -1,6 +1,8 @@
 import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.net.MalformedURLException;
@@ -42,10 +44,11 @@ public class GUI {
         titlePanel.add(howToPlay);
 
         start.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                //Button actions go here
-            }
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				System.out.println("fgo to story");
+				
+			}
         });
         
         assert background != null;
--- Inc.Cure/main.java
+++ Inc.Cure/main.java
@@ -2,9 +2,9 @@
 public class main {
 
 	public static void main(String[] args) {
-		//GUI game = new GUI();
-		LocationButtons buttons = new LocationButtons();
-		buttons.goToHospital();
+		new GUI();
+		//LocationButtons buttons = new LocationButtons();
+		//buttons.goToHospital();
 
 	}
 
